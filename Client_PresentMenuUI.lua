b98460d3373225c2aa7b70853724d04b77a6a2f3
Laurin Hohbach <laurinhohbach@web.de>
2024-03-04 23:17:33 +0100

    a

---
+++ Client_PresentMenuUI.lua
@@ -0,0 +1,17 @@
+require("Annotations");
+require("TextWriter");
+
+---Client_PresentMenuUI hook
+---@param rootParent RootParent
+---@param setMaxSize fun(width: number, height: number) # Sets the max size of the dialog
+---@param setScrollable fun(horizontallyScrollable: boolean, verticallyScrollable: boolean) # Set whether the dialog is scrollable both horizontal and vertically
+---@param game GameClientHook
+---@param close fun() # Zero parameter function that closes the dialog
+function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
+    ---@type VerticalLayoutGroup
+    local x = UI.CreateVerticalLayoutGroup(rootParent)
+    AddStringToUI(x,
+        "Lorem <#ff0000>ipsum dolor sit amet, consetetur sadipscing <#00ff00>elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        200);
+    print(x.GetPreferredWidth());
+end
